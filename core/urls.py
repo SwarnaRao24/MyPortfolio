bfebdbfa46d1be1dbbb6a4166d9df527bc93292e
Swarna Rao <chintal1@uwindsor.ca>
2026-05-27 23:13:08 -0400

    feat: Search Engine Optimization fixed

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,53 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+def robots_txt(request):
+    content = """User-agent: *
+Allow: /
+Disallow: /admin/
+
+Sitemap: https://theswarnaraogroup.com/sitemap.xml"""
+    return HttpResponse(content, content_type="text/plain")
+
+
+def sitemap_xml(request):
+    content = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://theswarnaraogroup.com/</loc>
+        <changefreq>monthly</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://theswarnaraogroup.com/history/</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://theswarnaraogroup.com/projects/</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://theswarnaraogroup.com/services/</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.9</priority>
+    </url>
+    <url>
+        <loc>https://theswarnaraogroup.com/contact/</loc>
+        <changefreq>yearly</changefreq>
+        <priority>0.7</priority>
+    </url>
+</urlset>"""
+    return HttpResponse(content, content_type="application/xml")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),       # <-- Fixed: Added the 's' to urls
-    path('', include('portfolio.urls')),   # <-- Fixed: Changed the slash '/' to a dot '.'
+    path('admin/', admin.site.urls),
+    path('robots.txt', robots_txt),
+    path('sitemap.xml', sitemap_xml),
+    path('', include('portfolio.urls')),
 ]
